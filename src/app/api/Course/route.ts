255b01902e0b727c3915a7c4e3393bdbcdf1bb1e
Motiullah Sajit <sajitmotiullah@gmail.com>
2024-03-15 00:55:57 +0600

    methods added

--- src/app/api/Course/route.ts
+++ src/app/api/Course/route.ts
@@ -6,23 +6,23 @@ export async function GET(req: any) {
     const client = await pool.connect();
     const url = await req.nextUrl.searchParams;
     const adminId = url.get("adminId");
-    const guardianId = url.get("guardianId");
+    const courseId = url.get("courseId");
     const action = await isAdmin(adminId);
 
     if (!action) {
       return Response.json({ message: "Access denied" });
     }
 
-    let query = "SELECT * FROM guardian";
+    let query = "SELECT * FROM course";
 
-    if (guardianId) {
+    if (courseId) {
       query += " WHERE id = $1";
-      const { rows } = await client.query(query, [guardianId]);
+      const { rows } = await client.query(query, [courseId]);
 
       client.release();
 
       if (rows.length === 0) {
-        return Response.json({ message: "Guardian not found" });
+        return Response.json({ message: "Course not found" });
       }
 
       return Response.json(rows[0]);
@@ -32,24 +32,23 @@ export async function GET(req: any) {
       client.release();
 
       if (rows.length === 0) {
-        return Response.json({ message: "No Guardian Exits" });
+        return Response.json({ message: "No Course Exits" });
       }
 
       return Response.json(rows);
     }
   } catch (error) {
-    console.error("Error fetching guardians:", error);
+    console.error("Error fetching courses:", error);
     return Response.json({ message: "Internal Server Error" });
   }
 }
 
 export async function POST(req: Request) {
   try {
-    const guardian = await req.json();
-    const { name, username, email, student_id, phone_number, address } =
-      guardian;
+    const course = await req.json();
+    const { name, course_code, description, credit, course_outline } = course;
 
-    if (!name || !username || !email || !student_id || !phone_number) {
+    if (!name || !course_code || !description || !credit || !course_outline) {
       return Response.json({ message: "Required fields are missing" });
     }
 
@@ -59,53 +58,46 @@ export async function POST(req: Request) {
         SELECT EXISTS (
           SELECT 1
           FROM information_schema.tables
-          WHERE table_name = 'guardian'
+          WHERE table_name = 'course'
         )`;
     const { rows } = await client.query(checkTableQuery);
     const tableExists = rows[0].exists;
 
     if (!tableExists) {
       const createTableQuery = `
-          CREATE TABLE guardian (
+          CREATE TABLE course (
             id SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
-            username VARCHAR(255) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            student_id INT NOT NULL,
-            phone_number VARCHAR(20),
-            address TEXT
+            course_code VARCHAR(20) UNIQUE NOT NULL,
+            description TEXT NOT NULL,
+            credit INT NOT NULL,
+            course_outline TEXT NOT NULL
           )`;
       await client.query(createTableQuery);
     }
 
     const insertQuery = `
-        INSERT INTO guardian (name, username, email, student_id, phone_number, address) 
-        VALUES ($1, $2, $3, $4, $5, $6)`;
+        INSERT INTO course (name, course_code, description, credit, course_outline) 
+        VALUES ($1, $2, $3, $4, $5)`;
     await client.query(insertQuery, [
       name,
-      username,
-      email,
-      student_id,
-      phone_number,
-      address,
+      course_code,
+      description,
+      credit,
+      course_outline,
     ]);
 
     client.release();
 
-    return Response.json({ message: "Guardian added successfully" });
+    return Response.json({ message: "Course added successfully" });
   } catch (error: any) {
     if (
       error.code === "23505" &&
-      error.constraint === "guardian_username_key"
+      error.constraint === "course_course_code_key"
     ) {
-      return Response.json({ message: "Username already exists" });
-    } else if (
-      error.code === "23505" &&
-      error.constraint === "guardian_email_key"
-    ) {
-      return Response.json({ message: "Email already exists" });
+      return Response.json({ message: "Course code already exists" });
     } else {
-      console.error("Error creating guardian:", error);
+      console.error("Error creating course:", error);
       return Response.json({ message: "Internal Server Error" });
     }
   }
@@ -113,9 +105,9 @@ export async function POST(req: Request) {
 
 export async function PUT(req: Request) {
   try {
-    const guardianData = await req.json();
-    const { id, name, username, email, student_id, phone_number, address } =
-      guardianData;
+    const courseData = await req.json();
+    const { id, name, course_code, description, credit, course_outline } =
+      courseData;
 
     if (!id) {
       return Response.json({ message: "ID parameter is required" });
@@ -123,12 +115,12 @@ export async function PUT(req: Request) {
 
     const client = await pool.connect();
 
-    const checkExistenceQuery = "SELECT * FROM guardian WHERE id = $1";
+    const checkExistenceQuery = "SELECT * FROM course WHERE id = $1";
     const { rows } = await client.query(checkExistenceQuery, [id]);
 
     if (rows.length === 0) {
       client.release();
-      return Response.json({ message: "Guardian not found" });
+      return Response.json({ message: "Course not found" });
     }
 
     const updateQueryParts = [];
@@ -141,33 +133,28 @@ export async function PUT(req: Request) {
       queryParams.push(name);
     }
 
-    if (username !== undefined) {
-      updateQueryParts.push(`username = $${queryIndex++}`);
-      queryParams.push(username);
+    if (course_code !== undefined) {
+      updateQueryParts.push(`course_code = $${queryIndex++}`);
+      queryParams.push(course_code);
     }
 
-    if (email !== undefined) {
-      updateQueryParts.push(`email = $${queryIndex++}`);
-      queryParams.push(email);
+    if (description !== undefined) {
+      updateQueryParts.push(`description = $${queryIndex++}`);
+      queryParams.push(description);
     }
 
-    if (student_id !== undefined) {
-      updateQueryParts.push(`student_id = $${queryIndex++}`);
-      queryParams.push(student_id);
+    if (credit !== undefined) {
+      updateQueryParts.push(`credit = $${queryIndex++}`);
+      queryParams.push(credit);
     }
 
-    if (phone_number !== undefined) {
-      updateQueryParts.push(`phone_number = $${queryIndex++}`);
-      queryParams.push(phone_number);
-    }
-
-    if (address !== undefined) {
-      updateQueryParts.push(`address = $${queryIndex++}`);
-      queryParams.push(address);
+    if (course_outline !== undefined) {
+      updateQueryParts.push(`course_outline = $${queryIndex++}`);
+      queryParams.push(course_outline);
     }
 
     const updateQuery = `
-      UPDATE guardian 
+      UPDATE course 
       SET ${updateQueryParts.join(", ")}
       WHERE id = $${queryIndex}`;
 
@@ -177,9 +164,9 @@ export async function PUT(req: Request) {
 
     client.release();
 
-    return Response.json({ message: "Guardian updated successfully" });
+    return Response.json({ message: "Course updated successfully" });
   } catch (error: any) {
-    console.error("Error updating guardian:", error);
+    console.error("Error updating course:", error);
     return Response.json({ message: "Internal Server Error" });
   }
 }
@@ -201,22 +188,22 @@ export async function DELETE(req: any) {
     if (!id) {
       client.release();
       return Response.json({
-        message: "Please provide a guardianId to delete a specific guardian",
+        message: "Please provide a courseId to delete a specific course",
       });
     }
 
-    const deleteQuery = "DELETE FROM guardian WHERE id = $1";
+    const deleteQuery = "DELETE FROM course WHERE id = $1";
     const { rowCount } = await client.query(deleteQuery, [id]);
 
     client.release();
 
     if (rowCount === 0) {
-      return Response.json({ message: "Guardian not found" });
+      return Response.json({ message: "Course not found" });
     }
 
-    return Response.json({ message: "Guardian deleted successfully" });
+    return Response.json({ message: "Course deleted successfully" });
   } catch (error) {
-    console.error("Error deleting guardian:", error);
+    console.error("Error deleting course:", error);
     return Response.json({ message: "Internal Server Error" });
   }
 }
--- src/app/api/student/route.ts
+++ src/app/api/student/route.ts
@@ -6,23 +6,23 @@ export async function GET(req: any) {
     const client = await pool.connect();
     const url = await req.nextUrl.searchParams;
     const adminId = url.get("adminId");
-    const facultyId = url.get("facultyId");
+    const studentId = url.get("studentId");
     const action = await isAdmin(adminId);
 
     if (!action) {
       return Response.json({ message: "Access denied" });
     }
 
-    let query = "SELECT * FROM faculty";
+    let query = "SELECT * FROM student";
 
-    if (facultyId) {
+    if (studentId) {
       query += " WHERE id = $1";
-      const { rows } = await client.query(query, [facultyId]);
+      const { rows } = await client.query(query, [studentId]);
 
       client.release();
 
       if (rows.length === 0) {
-        return Response.json({ message: "Faculty not found" });
+        return Response.json({ message: "Student not found" });
       }
 
       return Response.json(rows[0]);
@@ -32,38 +32,38 @@ export async function GET(req: any) {
       client.release();
 
       if (rows.length === 0) {
-        return Response.json({ message: "No Faculty Exits" });
+        return Response.json({ message: "No Student Exits" });
       }
 
       return Response.json(rows);
     }
   } catch (error) {
-    console.error("Error fetching faulty faculty data:", error);
+    console.error("Error fetching students:", error);
     return Response.json({ message: "Internal Server Error" });
   }
 }
 
 export async function POST(req: Request) {
   try {
-    const faculty = await req.json();
+    const student = await req.json();
     const {
       name,
       username,
       email,
-      qualifications,
-      department_id,
-      courses,
+      guardian_id,
       phone_number,
-    } = faculty;
+      address,
+      department,
+    } = student;
 
     if (
       !name ||
       !username ||
       !email ||
-      !qualifications ||
-      !department_id ||
-      !courses ||
-      !phone_number
+      !guardian_id ||
+      !phone_number ||
+      !address ||
+      !department
     ) {
       return Response.json({ message: "Required fields are missing" });
     }
@@ -71,55 +71,55 @@ export async function POST(req: Request) {
     const client = await pool.connect();
 
     const checkTableQuery = `
-      SELECT EXISTS (
-        SELECT 1
-        FROM information_schema.tables
-        WHERE table_name = 'faculty'
-      )`;
+          SELECT EXISTS (
+            SELECT 1
+            FROM information_schema.tables
+            WHERE table_name = 'student'
+          )`;
     const { rows } = await client.query(checkTableQuery);
     const tableExists = rows[0].exists;
 
     if (!tableExists) {
       const createTableQuery = `
-        CREATE TABLE faculty (
-          id SERIAL PRIMARY KEY,
-          name VARCHAR(255) NOT NULL,
-          username VARCHAR(255) UNIQUE NOT NULL,
-          email VARCHAR(255) UNIQUE NOT NULL,
-          qualifications TEXT,
-          department_id VARCHAR(50) NOT NULL,
-          courses TEXT[],
-          phone_number VARCHAR(20) NOT NULL
-        )`;
+            CREATE TABLE student (
+              id SERIAL PRIMARY KEY,
+              name VARCHAR(255) NOT NULL,
+              username VARCHAR(255) UNIQUE NOT NULL,
+              email VARCHAR(255) UNIQUE NOT NULL,
+              guardian_id INT NOT NULL,
+              phone_number VARCHAR(20),
+              address TEXT,
+              department VARCHAR(100)
+            )`;
       await client.query(createTableQuery);
     }
 
     const insertQuery = `
-      INSERT INTO faculty (name, username, email, qualifications, department_id, courses, phone_number) 
-      VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+          INSERT INTO student (name, username, email, guardian_id, phone_number, address, department) 
+          VALUES ($1, $2, $3, $4, $5, $6, $7)`;
     await client.query(insertQuery, [
       name,
       username,
       email,
-      qualifications,
-      department_id,
-      courses,
+      guardian_id,
       phone_number,
+      address,
+      department,
     ]);
 
     client.release();
 
-    return Response.json({ message: "Faculty added successfully" });
+    return Response.json({ message: "Student added successfully" });
   } catch (error: any) {
-    if (error.code === "23505" && error.constraint === "faculty_username_key") {
+    if (error.code === "23505" && error.constraint === "student_username_key") {
       return Response.json({ message: "Username already exists" });
     } else if (
       error.code === "23505" &&
-      error.constraint === "faculty_email_key"
+      error.constraint === "student_email_key"
     ) {
       return Response.json({ message: "Email already exists" });
     } else {
-      console.error("Error creating faculty data:", error);
+      console.error("Error creating student:", error);
       return Response.json({ message: "Internal Server Error" });
     }
   }
@@ -127,17 +127,17 @@ export async function POST(req: Request) {
 
 export async function PUT(req: Request) {
   try {
-    const facultyData = await req.json();
+    const studentData = await req.json();
     const {
       id,
       name,
       username,
       email,
-      qualifications,
-      department_id,
-      courses,
+      guardian_id,
       phone_number,
-    } = facultyData;
+      address,
+      department,
+    } = studentData;
 
     if (!id) {
       return Response.json({ message: "ID parameter is required" });
@@ -145,12 +145,12 @@ export async function PUT(req: Request) {
 
     const client = await pool.connect();
 
-    const checkExistenceQuery = "SELECT * FROM faculty WHERE id = $1";
+    const checkExistenceQuery = "SELECT * FROM student WHERE id = $1";
     const { rows } = await client.query(checkExistenceQuery, [id]);
 
     if (rows.length === 0) {
       client.release();
-      return Response.json({ message: "Faculty not found" });
+      return Response.json({ message: "Student not found" });
     }
 
     const updateQueryParts = [];
@@ -173,19 +173,9 @@ export async function PUT(req: Request) {
       queryParams.push(email);
     }
 
-    if (qualifications !== undefined) {
-      updateQueryParts.push(`qualifications = $${queryIndex++}`);
-      queryParams.push(qualifications);
-    }
-
-    if (department_id !== undefined) {
-      updateQueryParts.push(`department_id = $${queryIndex++}`);
-      queryParams.push(department_id);
-    }
-
-    if (courses !== undefined) {
-      updateQueryParts.push(`courses = $${queryIndex++}`);
-      queryParams.push(courses);
+    if (guardian_id !== undefined) {
+      updateQueryParts.push(`guardian_id = $${queryIndex++}`);
+      queryParams.push(guardian_id);
     }
 
     if (phone_number !== undefined) {
@@ -193,10 +183,20 @@ export async function PUT(req: Request) {
       queryParams.push(phone_number);
     }
 
+    if (address !== undefined) {
+      updateQueryParts.push(`address = $${queryIndex++}`);
+      queryParams.push(address);
+    }
+
+    if (department !== undefined) {
+      updateQueryParts.push(`department = $${queryIndex++}`);
+      queryParams.push(department);
+    }
+
     const updateQuery = `
-      UPDATE faculty 
-      SET ${updateQueryParts.join(", ")}
-      WHERE id = $${queryIndex}`;
+        UPDATE student 
+        SET ${updateQueryParts.join(", ")}
+        WHERE id = $${queryIndex}`;
 
     queryParams.push(id);
 
@@ -204,9 +204,9 @@ export async function PUT(req: Request) {
 
     client.release();
 
-    return Response.json({ message: "Faculty updated successfully" });
+    return Response.json({ message: "Student updated successfully" });
   } catch (error: any) {
-    console.error("Error updating faculty data:", error);
+    console.error("Error updating student:", error);
     return Response.json({ message: "Internal Server Error" });
   }
 }
@@ -228,22 +228,22 @@ export async function DELETE(req: any) {
     if (!id) {
       client.release();
       return Response.json({
-        message: "Please provide a facultyId to delete a specific faculty",
+        message: "Please provide a studentId to delete a specific student",
       });
     }
 
-    const deleteQuery = "DELETE FROM faculty WHERE id = $1";
+    const deleteQuery = "DELETE FROM student WHERE id = $1";
     const { rowCount } = await client.query(deleteQuery, [id]);
 
     client.release();
 
     if (rowCount === 0) {
-      return Response.json({ message: "Faculty not found" });
+      return Response.json({ message: "Student not found" });
     }
 
-    return Response.json({ message: "Faculty deleted successfully" });
+    return Response.json({ message: "Student deleted successfully" });
   } catch (error) {
-    console.error("Error deleting faculty:", error);
+    console.error("Error deleting student:", error);
     return Response.json({ message: "Internal Server Error" });
   }
 }
